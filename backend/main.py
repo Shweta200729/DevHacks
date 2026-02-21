ed1bc2f377de9f4649d74a4d061dfaa7e35d7a88
harnishdangi <harnishdangi@gmail.com>
2026-02-21 15:15:11 +0530

    feat: Initialize FastAPI application with Supabase client and a root endpoint.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE")
+key: str = os.environ.get("SUPABASE_ANON_KEY")
+
+if not url or not key:
+    raise ValueError("Supabase credentials not found in .env file")
+
+supabase: Client = create_client(url, key)
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
